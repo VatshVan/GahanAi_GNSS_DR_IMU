684892c6b865a425d4b265462c8702ede9c9becd
VatshVan <vatshvan.iitb@gmail.com>
2025-12-19 17:38:06 +0530

    chore: clean up build artifacts and logs, and update EKF core logic.

--- state_estimator/build/state_estimator/share/state_estimator/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/vatshvan/GahanAi_GNSS_DR_IMU/state_estimator/build/state_estimator"
